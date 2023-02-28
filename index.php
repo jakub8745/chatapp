9943d10f456809fcedf3f07b73423eb4ef1630ad
Blue Poit Art Gallery Project <53195000+jakub8745@users.noreply.github.com>
2023-02-28 00:32:25 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xml:lang="<?php echo $this->language; ?>"
+      lang="<?php echo $this->language; ?>" >
